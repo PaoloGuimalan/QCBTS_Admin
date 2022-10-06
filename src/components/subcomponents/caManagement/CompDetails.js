d6e296e9140129edf41509eae0eb26e83ded6adc
PaoloGuimalan <pauloportes.guimalan187@gmail.com>
2022-10-06 04:57:37 -0900

    Company Details Page Added features: Full functionality of Admins List.

--- src/components/subcomponents/caManagement/CompDetails.js
+++ src/components/subcomponents/caManagement/CompDetails.js
@@ -10,6 +10,10 @@ import { companyRecordState } from '../../../redux/actions'
 import DefaultIconComp from '../../../resources/defaultcompany.png'
 import MessageIcon from '@material-ui/icons/Message';
 import MailIcon from '@material-ui/icons/Mail'
+import CheckIcon from '@material-ui/icons/Check'
+import UncheckIcon from '@material-ui/icons/Close'
+import EditIcon from '@material-ui/icons/Edit'
+import { motion } from 'framer-motion'
 
 function CompDetails() {
 
@@ -43,6 +47,38 @@ function CompDetails() {
     })
   }
 
+  const updateCompanyStatus = (cmpAdID, sts) => {
+    Axios.post(`${URL}/admin/updatecompanystatus`, {
+        companyAdminID: cmpAdID,
+        status: sts
+    },{
+        headers:{
+            "x-access-token": localStorage.getItem("token")
+        }
+    }).then((response) => {
+        if(response.data.status){
+            fetchCompanyData();
+            // alertPrompt(true, `${cmpAdID} account has been ${sts? "Activated" : "Deactivated"}`)
+            // console.log(response.data.result);
+        }
+        else{
+            // alertPrompt(false, `${sts? "Activation" : "Deactivation"} of ${cmpAdID} account failed`)
+            // console.log(response.data.result);
+        }
+    }).catch((err) => {
+        // alertPrompt(false, `Update Request Error`)
+        console.log(err);
+    })
+  }
+
+  const redirectToMessages = (compadID) => {
+    navigate(`/home/messages/${compadID}`)
+  }
+
+  const redirectToCompAdDet = (compadID) => {
+    navigate(`/home/camanagement/companyAdminDetails/${compadID}`)
+  }
+
   return (
     <div id='div_addcompany'>
       <nav id='nav_addcompany'>
@@ -69,7 +105,7 @@ function CompDetails() {
                 </li>
                 <li>
                   <div id='div_header_contacts'>
-                    <button title='Message' className='btn_header_companydata'><MessageIcon /></button>
+                    <button title='Edit Details' className='btn_header_companydata'><EditIcon /></button>
                     <button title='Email' className='btn_header_companydata' onClick={() => { window.location.href = `mailto:${companyrecord.companydata.email}` }} ><MailIcon /></button>
                   </div>
                 </li>
@@ -128,6 +164,67 @@ function CompDetails() {
                   </nav>
                 </li>
               </nav>
+            </li>
+          </nav>
+        </li>
+        <li>
+          <nav id='nav_details_page'>
+            <li>
+              <div id='div_adminslist'>
+                <p className='label_informations_company'>Admins List</p>
+              </div>
+            </li>
+            <li>
+              <div className='div_lists'>
+                <table className='tbl_lists'>
+                  <tbody>
+                    <tr>
+                      <th className='th_adminlist'>Admin ID</th>
+                      <th className='th_adminlist'>Full Name</th>
+                      <th className='th_adminlist'>Email</th>
+                      <th className='th_adminlist'>Status</th>
+                      <th className='th_adminlist'>Navigations</th>
+                    </tr>
+                    {companyrecord.adminlist.map((records, i) => {
+                      return(
+                        <tr key={i}>
+                          <td className='td_adminlist' onClick={() => { redirectToCompAdDet(records.companyAdminID) }}>{records.companyAdminID}</td>
+                          <td className='td_adminlist' onClick={() => { redirectToCompAdDet(records.companyAdminID) }}>{records.companyAdmin.firstname} {records.companyAdmin.lastname}</td>
+                          <td className='td_adminlist'><a className='link_conf_lists' href={`mailto:${records.email}`}>{records.email}</a></td>
+                          <motion.td
+                          animate={{
+                            color: records.status? "green" : "red"
+                          }} 
+                          className='td_adminlist'>{records.status? "Activated" : "Deactivated"}</motion.td>
+                          <td>
+                            <button onClick={() => {
+                              updateCompanyStatus(records.companyAdminID, records.status? false : true)
+                            }}
+                            className='btns_list'>{records.status? <UncheckIcon style={{fontSize: "15px"}} /> : <CheckIcon style={{fontSize: "15px"}} />}</button>
+                            <button className='btns_list' onClick={() => { redirectToMessages(records.companyAdminID) }}><MessageIcon style={{fontSize: "15px"}} /></button>
+                          </td>
+                        </tr>
+                      )
+                    })}
+                  </tbody>
+                </table>
+              </div>
+            </li>
+          </nav>
+        </li>
+        <li>
+          <nav id='nav_details_page'>
+            <li>
+              <div id='div_adminslist'>
+                <p className='label_informations_company'>Drivers List</p>
+              </div>
+            </li>
+            <li>
+              <div className='div_lists'>
+                <p>Hello</p>
+                <p>Hello</p>
+                <p>Hello</p>
+              </div>
             </li>
           </nav>
         </li>
