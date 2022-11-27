651edd04c36d16814046c4c4285df4a8c81add33
PaoloGuimalan <pauloportes.guimalan187@gmail.com>
2022-11-26 22:09:32 -0900

    Cyclis url initialization for server switch up

--- src/json/urlconfig.js
+++ src/json/urlconfig.js
@@ -1,3 +1,3 @@
-export const URL = 'https://qcbtsserver.herokuapp.com';
-// export const URL = 'https://qcbtsserver.cyclic.app';
+// export const URL = 'https://qcbtsserver.herokuapp.com';
+export const URL = 'https://qcbtsserver.cyclic.app';
 // export const URL = 'http://localhost:3001';
