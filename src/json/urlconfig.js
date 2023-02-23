675e50b063855f4d4ae82a7da52342fd5cd12780
PaoloGuimalan <pauloportes.guimalan187@gmail.com>
2023-02-23 06:56:26 -0900

    URL and EXT_URL set to render servers

--- src/json/urlconfig.js
+++ src/json/urlconfig.js
@@ -2,7 +2,8 @@
 // export const URL = 'https://qcbtsserver.cyclic.app';
 // export const URL = 'http://localhost:3001';
 // export const URL = 'https://paologuimalan-ideal-meme-4rxxgq7px4g25j9v-3001.preview.app.github.dev';
-// export const URL = 'https://qcbtsserver.onrender.com'
-export const URL = 'https://qcbtsserver.up.railway.app'
+export const URL = 'https://qcbtsserver.onrender.com'
+// export const URL = 'https://qcbtsserver.up.railway.app'
 
-export const EXT_URL = 'https://transpotrackserver-production.up.railway.app'
+// export const EXT_URL = 'https://transpotrackserver-production.up.railway.app'
+export const EXT_URL = "https://transpotrackserver.onrender.com"
