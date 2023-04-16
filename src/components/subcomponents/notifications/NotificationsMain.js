191d9cd2456083fc5f73f77c874eb52e8dee8ff6
PaoloGuimalan <pauloportes.guimalan187@gmail.com>
2023-04-16 02:37:06 -0900

    loader icon and number of system activities per userType and refresh button has been Added

--- src/components/subcomponents/notifications/NotificationsMain.js
+++ src/components/subcomponents/notifications/NotificationsMain.js
@@ -3,6 +3,7 @@ import { Link, useLocation, useParams } from 'react-router-dom'
 import '../../../styles/subcomponents/NotificationsMain.css'
 import BellIcon from '@material-ui/icons/Notifications'
 import ReqIcon from '@material-ui/icons/NoteAdd'
+import LoaderIcon from '@material-ui/icons/Sync'
 import { useDispatch, useSelector } from 'react-redux'
 import { motion } from 'framer-motion'
 import Axios from 'axios'
@@ -16,8 +17,14 @@ function NotificationsMain() {
   const params = useLocation()
 
   const [selectedParam, setselectedParam] = useState("System Admin")
+  const [loadingsystemactivities, setloadingsystemactivities] = useState(true)
 
   useEffect(() => {
+    initSystemActivities()
+  },[])
+
+  const initSystemActivities = () => {
+    setloadingsystemactivities(true)
     Axios.get(`${URL}/admin/systemActivities`,{
       headers:{
         "x-access-token": localStorage.getItem("token")
@@ -25,11 +32,12 @@ function NotificationsMain() {
     }).then((response) => {
       if(response.data.status){
         dispatch({type: SET_SYSTEM_ACTIVITIES_LIST, systemactivitieslist: response.data.result})
+        setloadingsystemactivities(false)
       }
     }).catch((err) => {
       console.log(err)
     })
-  },[])
+  }
 
   // useEffect(() => {
   //   // console.log(params.pathname.split("/")[3])
@@ -44,7 +52,30 @@ function NotificationsMain() {
   return (
     <div id='div_notifications_main'>
       <div id='div_navigations_panel'>
-        <p id='p_navigations_label'><BellIcon style={{ fontSize: "17px" }} /> System Activities</p>
+        <p id='p_navigations_label'><BellIcon style={{ fontSize: "17px" }} /> System Activities 
+          {loadingsystemactivities? (
+            <motion.div
+            animate={{
+              rotate: -360
+            }}
+            transition={{
+              duration: 1,
+              repeat: Infinity
+            }}
+            id='div_loader_icon'>
+              <LoaderIcon style={{ fontSize: "20px", color: "grey" }} />
+            </motion.div>
+          ) : (
+            <motion.div
+            title='Refresh'
+            onClick={() => {
+              initSystemActivities()
+            }}
+            id='div_loader_icon'>
+              <LoaderIcon style={{ fontSize: "20px", color: "grey" }} />
+            </motion.div>
+          )}
+        </p>
         <div id='div_navigations_container'>
           <motion.button
           animate={{
@@ -54,7 +85,9 @@ function NotificationsMain() {
           onClick={() => {
             setselectedParam("System Admin")
           }}
-          className='link_icons_navigations'><BellIcon style={{ fontSize: "17px" }} />System Admin</motion.button>
+          className='link_icons_navigations'><BellIcon style={{ fontSize: "17px" }} />System Admin {
+            loadingsystemactivities? null : (`(${systemactivitieslist.filter((usrt, j) => usrt.userType == "System Admin").length})`)
+          }</motion.button>
           <motion.button
           animate={{
             backgroundColor: selectedParam == "Driver"? "orange" : "transparent",
@@ -63,7 +96,9 @@ function NotificationsMain() {
           onClick={() => {
             setselectedParam("Driver")
           }}
-          className='link_icons_navigations'><BellIcon style={{ fontSize: "17px" }} />Drivers</motion.button>
+          className='link_icons_navigations'><BellIcon style={{ fontSize: "17px" }} />Drivers {
+            loadingsystemactivities? null : (`(${systemactivitieslist.filter((usrt, j) => usrt.userType == "Driver").length})`)
+          }</motion.button>
           <motion.button
           animate={{
             backgroundColor: selectedParam == "Commuter"? "orange" : "transparent",
@@ -72,7 +107,9 @@ function NotificationsMain() {
           onClick={() => {
             setselectedParam("Commuter")
           }}
-          className='link_icons_navigations'><BellIcon style={{ fontSize: "17px" }} />Commuters</motion.button>
+          className='link_icons_navigations'><BellIcon style={{ fontSize: "17px" }} />Commuters {
+            loadingsystemactivities? null : (`(${systemactivitieslist.filter((usrt, j) => usrt.userType == "Commuter").length})`)
+          }</motion.button>
           {/* <Link to='/home/notifications' className='link_icons_navigations' style={{
             backgroundColor: selectedParam == "notif"? "#ffbf00" : "transparent",
             color: selectedParam == "notif"? "white" : "black"
@@ -88,29 +125,40 @@ function NotificationsMain() {
           <p id='div_systemactivites_result_label'>{selectedParam} Activities</p>
         </div>
         <div id='div_systemactivites_result_list'>
-          {systemactivitieslist.filter((usrt, j) => usrt.userType == selectedParam).map((sal, i) => {
-            return(
-              <div key={i} className='div_systemactivities_result_indv'>
-                <div className='div_result_indv_containers'>
-                  <p id='p_systemactivity_id'>{sal.activityID}</p>
-                  <p id='p_sa_commuterid'>{sal.userID}</p>
-                  <p id='p_sa_activity'>{sal.action}</p>
-                  <p id='p_sa_userType'>{sal.userType}</p>
+          {systemactivitieslist.filter((usrt, j) => usrt.userType == selectedParam).length != 0? (
+            systemactivitieslist.filter((usrt, j) => usrt.userType == selectedParam).map((sal, i) => {
+              return(
+                <div key={i} className='div_systemactivities_result_indv'>
+                  <div className='div_result_indv_containers'>
+                    <p id='p_systemactivity_id'>{sal.activityID}</p>
+                    <p id='p_sa_commuterid'>{sal.userID}</p>
+                    <p id='p_sa_activity'>{sal.action}</p>
+                    <p id='p_sa_userType'>{sal.userType}</p>
+                  </div>
+                  <div className='div_result_indv_containers'>
+                    <p id='p_sa_platform'>{sal.platform}</p>
+                    <p id='p_sa_date'>
+                      <span className='span_datetime_labels'>Date Recorded: </span>
+                      <span>{sal.dateCommited.dateRecorded}</span>
+                    </p>
+                    <p id='p_sa_time'>
+                      <span className='span_datetime_labels'>Time Recorded: </span>
+                      <span>{sal.dateCommited.timeRecorded}</span>
+                    </p>
+                  </div>
                 </div>
-                <div className='div_result_indv_containers'>
-                  <p id='p_sa_platform'>{sal.platform}</p>
-                  <p id='p_sa_date'>
-                    <span className='span_datetime_labels'>Date Recorded: </span>
-                    <span>{sal.dateCommited.dateRecorded}</span>
-                  </p>
-                  <p id='p_sa_time'>
-                    <span className='span_datetime_labels'>Time Recorded: </span>
-                    <span>{sal.dateCommited.timeRecorded}</span>
-                  </p>
-                </div>
-              </div>
-            )
-          })}
+              )
+            })
+          ) : (
+            <div id='div_no_systemactivities_display'>
+              {loadingsystemactivities? (
+                <p>Loading {selectedParam} Activities...</p>
+              ) : (
+                <p>No {selectedParam} Activities yet</p>
+              )}
+            </div>
+          )
+          }
         </div>
       </div>
     </div>
