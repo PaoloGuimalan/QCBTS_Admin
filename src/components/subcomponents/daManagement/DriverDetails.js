f0ebfad2d9edbc251579ac67998b031fd1965859
PaoloGuimalan <pauloportes.guimalan187@gmail.com>
2023-05-01 22:07:43 -0900

    Drivers Profile Page is now functional but without Create Report functionality yet

--- src/components/subcomponents/daManagement/DriverDetails.js
+++ src/components/subcomponents/daManagement/DriverDetails.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../../../styles/subcomponents/DriverDetails.css'
 import { useNavigate, useParams } from 'react-router-dom'
 import BackIcon from '@material-ui/icons/KeyboardArrowLeft'
@@ -11,6 +11,61 @@ function DriverDetails() {
   const params = useParams()
   const navigate = useNavigate()
   const driverID = params["driverID"]
+
+  const [driverprofiledata, setdriverprofiledata] = useState({
+    userID: "",
+    companyID: "",
+    userType: "",
+    firstName: "",
+    middleName: "",
+    lastName: "",
+    mobileNumber: 0,
+    email: "",
+    dlicense: "",
+    age: 0,
+    dateRegistered: "",
+    timeRegistered: "",
+    status: null,
+    locationSharing: null,
+    bus: {
+      busID: "",
+      companyID: "",
+      driverID: "",
+      busModel: "",
+      plateNumber: "",
+      capacity: "",
+      busNo: 0
+    },
+    routeData: {
+      routeID: "",
+      routeName: "",
+      dateAdded: "",
+      addedBy: "",
+      companyID: "",
+      privacy: null,
+      status: null
+    }
+  })
+
+  useEffect(() => {
+    initDriverProfile()
+  },[])
+
+  const initDriverProfile = () => {
+    Axios.get(`${URL}/admin/getDriverProfile/${driverID}`,{
+      headers:{
+        "x-access-token": localStorage.getItem("token")
+      }
+    }).then((response) => {
+      if(response.data.status){
+        //fetch driver profile data
+        setdriverprofiledata(response.data.result[0])
+        // console.log(response.data.result)
+      }
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
 
   return (
     <div id='div_driver_details'>
@@ -29,19 +84,74 @@ function DriverDetails() {
               <img src={DefaultImg} id='img_default_container' />
             </div>
             <div id='div_dd_name_container'>
-              <p className='p_dd_name_labels'>Hello World</p>
-              <p className='p_dd_name_labels'>Hello World</p>
+              <p className='p_dd_name_labels'>{driverprofiledata.firstName} {driverprofiledata.middleName == "N/A" || driverprofiledata.middleName == ""? "" : driverprofiledata.middleName} {driverprofiledata.lastName}</p>
+              <p className='p_dd_name_labels'>{driverprofiledata.email}</p>
             </div>
             <div id='div_dd_button_report_container'>
               <button id='btn_create_report'>Create Report</button>
             </div>
           </div>
           <div className='div_dd_content_containers'>
-            <p>Hello World</p>
+            <div id='div_dd_aci_content_main'>
+              <p className='p_dd_container_labels'>Account Information</p>
+              <div id='div_dd_aci_container'>
+                <div className='div_dd_aci_per_data_container'>
+                  <p className='p_dd_aci_data_labels'>Driver ID</p>
+                  <p className='p_dd_aci_data_content'>{driverprofiledata.userID}</p>
+                </div>
+                <div className='div_dd_aci_per_data_container'>
+                  <p className='p_dd_aci_data_labels'>Driver's License</p>
+                  <p className='p_dd_aci_data_content'>{driverprofiledata.dlicense}</p>
+                </div>
+                <div className='div_dd_aci_per_data_container'>
+                  <p className='p_dd_aci_data_labels'>Company ID</p>
+                  <p className='p_dd_aci_data_content'>{driverprofiledata.companyID}</p>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className='div_dd_content_containers'>
-            <p>Hello World</p>
-          </div>
+          {driverprofiledata.routeData != null? (
+            <div className='div_dd_content_containers'>
+              <div id='div_dd_aci_content_main'>
+                <p className='p_dd_container_labels'>Driver Route</p>
+                <div id='div_dd_aci_container'>
+                  <div className='div_dd_aci_per_data_container'>
+                    <p className='p_dd_aci_data_labels'>Route ID</p>
+                    <p className='p_dd_aci_data_content'>{driverprofiledata.routeData.routeID}</p>
+                  </div>
+                  <div className='div_dd_aci_per_data_container'>
+                    <p className='p_dd_aci_data_labels'>Route Name</p>
+                    <p className='p_dd_aci_data_content'>{driverprofiledata.routeData.routeName}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ) : null}
+          {driverprofiledata.bus != null? (
+            <div className='div_dd_content_containers'>
+              <div id='div_dd_aci_content_main'>
+                <p className='p_dd_container_labels'>Bus Assigned</p>
+                <div id='div_dd_aci_container'>
+                  <div className='div_dd_aci_per_data_container'>
+                    <p className='p_dd_aci_data_labels'>Bus ID</p>
+                    <p className='p_dd_aci_data_content'>{driverprofiledata.bus.busID}</p>
+                  </div>
+                  <div className='div_dd_aci_per_data_container'>
+                    <p className='p_dd_aci_data_labels'>Plate Number</p>
+                    <p className='p_dd_aci_data_content'>{driverprofiledata.bus.plateNumber}</p>
+                  </div>
+                  <div className='div_dd_aci_per_data_container'>
+                    <p className='p_dd_aci_data_labels'>Bus No.</p>
+                    <p className='p_dd_aci_data_content'>{driverprofiledata.bus.busNo == 0? "unassigned" : driverprofiledata.bus.busNo}</p>
+                  </div>
+                  <div className='div_dd_aci_per_data_container'>
+                    <p className='p_dd_aci_data_labels'>Capacity</p>
+                    <p className='p_dd_aci_data_content'>{driverprofiledata.bus.capacity}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ) : null}
         </li>
       </nav>
     </div>
